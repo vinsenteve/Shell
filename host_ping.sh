7aed8f0869244e0670b6dcb682167c800ea8c0b9
vinsenteve <33597161+vinsenteve@users.noreply.github.com>
2017-12-23 13:56:44 +0800

    Update host_ping.sh

--- host_ping.sh
+++ host_ping.sh
@@ -1,5 +1,17 @@
-#!/bin/bash 		
-#
+#!/bin/bash
+#------------------------------------------#
+# FileName:             host_ping.sh
+# Revision:             1.1.0
+# Date:                 2017-07-14 04:55:33
+# Author:               vinsent
+# Email:                hyb_admin@163.com
+# Website:              www.vinsent.cn
+# Description:          This script can achieve ssh password-free login, 
+#                       and can be deployed in batches, configuration
+#------------------------------------------#
+# Copyright:            2017 vinsent
+# License:              GPL 2+
+#------------------------------------------#
 read -p "Please enter a IPv4 address:" ip
 ping -c1 -w 1 $ip &> /dev/null
 if [ $? -eq 0 ];then 												
