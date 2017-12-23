5dc6dedcc8bcf79ebf0ffb4a4b6a7d7427249c40
vinsenteve <33597161+vinsenteve@users.noreply.github.com>
2017-12-23 14:43:05 +0800

    Update matrix.sh

--- matrix.sh
+++ matrix.sh
@@ -18,13 +18,13 @@ declare -a arry
 arry=([00]=1 [01]=2 [02]=3 [10]=4 [11]=5 [12]=6 [20]=7 [21]=8 [22]=9)
 echo -e "  ${arry[*]:0:3}\n  ${arry[*]:3:3}\n  ${arry[*]: -3}"
 for i in `seq 1 2`;do
-	  for j in `seq 0 1`;do
-				if [ $i -ne $j ];then
-						t=${arry[$i$j]}
-						arry[$i$j]=${arry[$j$i]}
-						arry[$j$i]=$t
-				fi
-		done
+    for j in `seq 0 1`;do
+        if [ $i -ne $j ];then
+            t=${arry[$i$j]}
+            arry[$i$j]=${arry[$j$i]}
+            arry[$j$i]=$t
+        fi
+    done
 done
 echo -e " -------"
 echo -e "  ${arry[*]:0:3}\n  ${arry[*]:3:3}\n  ${arry[*]: -3}"
